e772fe62e3269a69d347a1fba15200f225962898
iosebi <Ioseb-1999>
2024-02-27 18:53:15 +0400

    Added donorbox

--- src/components/donorbox/donorbox.js
+++ src/components/donorbox/donorbox.js
@@ -1,6 +1,6 @@
-export default function DonorBox({width}) {
+export default function DonorBox({ width }) {
   const iframeCode = `
-  <iframe src="https://donorbox.org/embed/david-bezhuashvili-education-foundation-dbef" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height=600px" width=${width} style=" min-width: 250px; max-height:none!important; border-radius: 10px; overflow-x: auto; border: none !important;" id="dbefDonate"></iframe>
+  <script src="https://donorbox.org/widget.js" paypalExpress="true"></script><iframe src="https://donorbox.org/embed/act-georgia-1?language=en" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="600px" width=${width}  style=" min-width: 250px; max-height:none!important; border-radius: 10px; overflow-x: auto; border: none !important;" id="dbefDonate"></iframe>
     `;
 
   return <div dangerouslySetInnerHTML={{ __html: iframeCode }} />;
