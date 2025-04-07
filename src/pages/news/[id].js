aaa803a369be4a97cf6a4a8c53441003270dad9b
iosebi1999 <iosebi@hlwgroup.dev>
2025-04-07 17:48:54 +0400

    reverted changes

--- src/pages/news/[id].js
+++ src/pages/news/[id].js
@@ -1,53 +1,11 @@
 import Head from "next/head";
 import NewsDetails from "src/components/news-details/news-details";
 import { Layout } from "src/layouts/main/layout";
-import { db } from "../../lib/firebase-admin";
 
-export async function getServerSideProps(context) {
-  const { id } = context.params;
-
-  try {
-    const docRef = db.collection("news").doc(id);
-    const docSnap = await docRef.get();
-
-    if (!docSnap.exists) {
-      return { notFound: true };
-    }
-
-    const news = {
-      id: docSnap.id,
-      ...docSnap.data(),
-    };
-
-    delete news["created_at"];
-    delete news["updated_at"];
-
-    console.log("NEWS:", news);
-
-    return {
-      props: { news },
-    };
-  } catch (error) {
-    console.error("Error fetching news:", error);
-    return { notFound: true };
-  }
-}
-
-const NewsPage = ({ news }) => {
+const NewsPage = () => {
   return (
     <>
-      <Head>
-        <title>{news.title_ka}</title>
-        <meta name="description" content={news.description_ka} />
-        <meta name="image" content={news?.photos?.[0]?.url} />
-        <meta property="og:title" content={news.title_ka} />
-        <meta property="og:description" content={news.description_ka} />
-        <meta
-          property="og:image"
-          content={news?.photos?.[0]?.url || "/default-og.png"}
-        />
-      </Head>
-      <NewsDetails news={news} />
+      <NewsDetails />
     </>
   );
 };
