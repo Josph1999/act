2ba5d80548713a1a91985db998565bf9312c6e0c
iosebi1999 <iosebi@hlwgroup.dev>
2025-04-07 16:12:31 +0400

    Added firebase admin

--- src/firebase/firebase-admin.js
+++ src/firebase/firebase-admin.js
@@ -4,8 +4,6 @@ if (!admin.apps.length) {
 
   const creds = JSON.parse(process.env.ADMIN_SERVICE_ACCOUNT);
 
-  console.log('CREDS:', creds)
-
   admin.initializeApp({
     credential: admin.credential.cert(creds),
   });
